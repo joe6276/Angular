e9382e1dc7403733ae506236b3650f3ab2b0841f
Jonathan Ndambuki <joendambuki16@gmail.com>
2024-06-20 11:57:46 +0300

    directives

--- getting_started/src/app/app.component.ts
+++ getting_started/src/app/app.component.ts
@@ -8,57 +8,22 @@ import { ShortenDescription } from './Pipes/ShortenDescription';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from './Pipes/filter.pipe';
 import { ProductComponent } from './product/product.component';
+import { DisplayProductsComponent } from './display-products/display-products.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ChangeBackgroundDirective } from '../Directives/changeBackground.directive';
+import { ChangeBgDirective } from '../Directives/change-bg.directive';
+import { HoverDirective } from '../Directives/hover.directive';
+import { OurIfDirective } from '../Directives/our-if.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddPeopleComponent,FormsModule,ProductComponent, CommonModule, DisplayComponent, ShortenDescription, FilterPipe],
+  imports: [RouterOutlet, HoverDirective, OurIfDirective, ChangeBgDirective,ChangeBackgroundDirective, FormsModule, CommonModule,DisplayProductsComponent,AddProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'getting_started';
-  searchTerm=""
-  products:Product[]=[
-    {
-      Id:1,
-      Name:"Laptop",
-      Description:"Lenovo Laptop with 16 Gb RAM ",
-      price:3000
-    },
 
-    {
-      Id:2,
-      Name:"Flash Disk",
-      Description:" SanDisk Flash ",
-      price:30
-    },
-    {
-      Id:3,
-      Name:" HP Mouse",
-      Description:"Mouse",
-      price:30
-    }
-  ]
+condition=false
 
-  addProduct(){
-    this.products=[]
-      }
-  todaysDate= new Date()
-  money =330030
-  percent =0.8
-
-  greeting= new Promise<string>((resolve, reject)=>{
-    setTimeout(()=>{
-      resolve("Hello There")
-    }, 2000)
-  })
-  peoples:{Name:string,Age:number}[]=[{Name:"John", Age:12}]
-  addNewPerson(eventData:{Name:string, Age:number}){
-    this.peoples.push(eventData)
-  }
-
-  deletePerson(eventData:{id:number}){
-    this.peoples.splice(eventData.id, 1)
-  }
 }
